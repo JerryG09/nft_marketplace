db9a44d5f3dcb60b8a30673cfe8639e6f710e5e6
JeremiahAfriprud <87825568+JeremiahAfriprud@users.noreply.github.com>
2022-01-20 09:06:52 +0100

    setup

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 contract NFTMarket is ReentrancyGuard {
   using Counters for Counters.Counter;
