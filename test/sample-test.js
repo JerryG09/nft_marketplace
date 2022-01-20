6ef43ce642c169af4e33e08725edae54110484d2
JeremiahAfriprud <87825568+JeremiahAfriprud@users.noreply.github.com>
2022-01-20 09:08:54 +0100

    added test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,6 +1,3 @@
-// const { expect } = require("chai");
-
-const { ethers } = require("hardhat");
 describe("NFTMarket", function() {
   it("Should create and execute market sales", async function() {
     const Market = await ethers.getContractFactory("NFTMarket")
