fdb3300fb2a056ccb3c3836b383376aa9d6776ab
Jeremiah Oyebamiji <phemmy09@gmail.com>
2022-03-12 06:19:16 +0100

    updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -9,20 +9,20 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    /*
-    mumbai: {
-      // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [privateKey]
-    },
-    matic: {
-      // Infura
-      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
-      url: "https://rpc-mainnet.maticvigil.com",
-      accounts: [privateKey]
-    }
-    */
+    
+    // mumbai: {
+    //   // Infura
+    //   url: `https://polygon-mumbai.infura.io/v3/${infuraId}`,
+    //   // url: "https://rpc-mumbai.matic.today",
+    //   accounts: [privateKey]
+    // },
+    // matic: {
+    //   // Infura
+    //   url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+    //   // url: "https://rpc-mainnet.maticvigil.com",
+    //   accounts: [privateKey]
+    // }
+  
   },
   solidity: {
     version: "0.8.4",
--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function Marketplace({ Component, pageProps }) {
         <p className="text-4xl font-bold">Metaverse Marketplace</p>
         <div className="flex mt-4">
           <Link href="/">
-            <a className="mr-4 text-pink-500">
+            <a className="mr-4 text-pink-500 hover:text-pink-700">
               Home
             </a>
           </Link>
